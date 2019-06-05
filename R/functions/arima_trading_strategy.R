9c0a39dc5434f8eebe12e256bce512c2451fc4f7
Paul Nheera <pnheera@iqbusiness.net>
2019-06-05 14:39:26 +0200

    Initial commit, with the arima_trading_strategy function.

---
+++ R/functions/arima_trading_strategy.R
@@ -0,0 +1,28 @@
+#
+# ARIMA Trading Strategy:
+#
+
+#
+# Generates a signal from the ARIMA trading strategy
+#
+
+# Load Libraries: ----
+library(forecast)
+library(quantmod)
+
+arima_trading_strategy <- function(ts,lookback){
+  
+  ts <- tail(ts,lookback)
+  
+  model = auto.arima(ts)
+  pred = forecast(model,1) # Forecast one step ahead.
+  
+  # Criteria:
+  ret_mean = (pred/last(ts))-1
+  
+  # Signal:
+  signal <- ifelse(ret_mean >0,1,-1)
+  
+  return(signal)
+  
+}
